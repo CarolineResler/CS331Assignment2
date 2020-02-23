82235c744354716ec7b096cb7ae9f4a139b4d05e
CarolineResler <carolineresler2@gmail.com>
2020-02-22 17:00:40 -0800

    first

---
+++ src/DivideAndConquer.java
@@ -0,0 +1,31 @@
+/* CS331
+ * Created by Caroline Resler
+ * Due Oct 1st
+ */
+/**
+ *
+ * @author Caroline
+ */
+import static java.lang.Integer.MAX_VALUE;
+import java.util.*;
+
+// C++ program to get least cost path in a grid from
+// top-left to bottom-right
+
+
+// structure for information of each cell
+public class cell
+{
+public int x;
+public int y;
+public int distance;
+public cell(int x, int y, int distance)
+{
+	this.x = x;
+	this.y = y;
+	this.distance = distance;
+}
+}
+
+
+}
